c93841a0708f871c2dadf959fb04f6c0c7518719
unreact-hatano <mizutama.hahaha@gmail.com>
2025-05-25 15:42:14 +0900

    whoamiページ修正

--- src/components/whoami/Links.tsx
+++ src/components/whoami/Links.tsx
@@ -11,13 +11,13 @@ const Links = (props: Props) => {
     <div className={className}>
       <h3 className="text-secondary">Link</h3>
       <ul className="mt-2 text-body">
-        <li className="flex items-center gap-2">
+        <li className="flex items-center gap-2 hover:text-pink-300 transition">
           <FaGithub />
           <a href="https://github.com/hatano299" target="_blank" rel="noopener noreferrer">
             GitHub
           </a>
         </li>
-        <li className="flex items-center gap-2 mt-1">
+        <li className="flex items-center gap-2 mt-1 hover:text-pink-300 transition">
           <FaInstagram />
           <a href="https://www.instagram.com/hatano.299/" target="_blank" rel="noopener noreferrer">
             Instagram
